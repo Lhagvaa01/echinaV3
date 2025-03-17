b5c84ad449b2f266f386aac9883ba274c80ef644
Lhagvaa <zayalhagva6@gmail.com>
2025-03-17 16:59:40 +0800

    Airkacc add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
+import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,18 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    server: {
+        host: '0.0.0.0',   // Бүх IP хандахыг зөвшөөрнө
+        port: 5070,
+        strictPort: true,
+        cors: true,         // CORS зөвшөөрнө
+        allowedHosts: ['www.airkacc.mn'], // Зөвшөөрөгдсөн домэйнүүд
+        hmr: {
+            clientPort: 443 // HMR-ийг зөвшөөрнө (SSL тохируулсан бол)
+        }
+    },
     preview: {
         host: '0.0.0.0',
-        port: 5085
+        port: 5070
     }
 })
